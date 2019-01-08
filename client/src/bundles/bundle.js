5da31dd92edcac598b7fec95fb05ecd30af13499
Dylan Wagstaff <dylan@silverstripe.com>
2019-01-08 16:19:25 +1300

    Separate the concerns of presenting options, and selecting their source
    In order to tidy up code base and make it a bit more readable, make the
    selector for the source of presented options on a CKAN ResourceFilter
    Dropdown both not hard coded to the select, and a matter of its own
    concern (as opposed to being coupled into the logic of presenting the
    options).

--- client/src/bundles/bundle.js
+++ client/src/bundles/bundle.js
@@ -1,5 +1,6 @@
 require('legacy/CKANResourceLoaderField-entwine.js');
 require('legacy/CKANPresentedOptions-entwine.js');
 require('legacy/CKANResultConditionsField-entwine.js');
+require('legacy/CKANResourceFilterForm-entwine.js');
 require('legacy/ResourceFilter.js');
 require('boot');
